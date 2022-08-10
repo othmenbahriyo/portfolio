75cc4d9c105e6c5f3330074eab3a89674169b1b7
othmenbahriyo <othmeneb@gmail.com>
2022-08-10 09:27:43 +0100

    up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -159,9 +159,9 @@ const RESOURCES = {
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "7508ab915d3bfc418e50e2c1c53456d2",
 "icons/Icon-512.png": "7508ab915d3bfc418e50e2c1c53456d2",
-"index.html": "9df3e881f8eb0883a4f356d824752bd2",
-"/": "9df3e881f8eb0883a4f356d824752bd2",
-"main.dart.js": "f3b4b611a8d085eee4dea7f61936ae62",
+"index.html": "09b2e8c033ff5e1b8a0cf7d77d21764a",
+"/": "09b2e8c033ff5e1b8a0cf7d77d21764a",
+"main.dart.js": "3e87f66ab5038a85f973750fed0b7efe",
 "manifest.json": "49784c7ae91d2aad3874685b094eef3c",
 "version.json": "a29e8b22a08aa0274b027931eec216c4"
 };
