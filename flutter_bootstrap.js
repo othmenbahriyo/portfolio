0bab1e5e31a1c8ce83393c4146389febf3c04f9c
othmen.bahri <othmen.bahri@transgourmet.fr>
2025-04-07 12:39:20 +0200

    up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3975242449"
+    serviceWorkerVersion: "3965103706"
   }
 });
